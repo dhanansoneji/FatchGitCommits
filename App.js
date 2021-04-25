8b595a47038ad8f0003f7ec33e1db3eae861c95d
dhanansoneji <dhanansoneji@gmail.com>
2021-04-25 16:52:49 -0500

    Called and Console Loged all the commits from GitHub API

--- App.js
+++ App.js
@@ -21,12 +21,41 @@ import {create} from 'apisauce';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
+const api = create({
+  baseURL: 'https://api.github.com',
+  headers: {Accept: 'application/vnd.github.v3+json'},
+});
+
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  const [commits, setCommits] = useState([]);
+
+  const getCommits = () => {
+    api
+      .get('/repos/dhanansoneji/Cats-Care/commits')
+      .then(response =>
+        response.data.map(e => {
+          return {
+            commitSHA: e.sha,
+            author: e.commit.author.name,
+            message: e.commit.message,
+          };
+        }),
+      )
+      .then(response => {
+        setCommits(response);
+        console.log(response);
+      });
+  };
+
+  useEffect(() => {
+    getCommits();
+  }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
