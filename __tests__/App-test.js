d7d914f9b9a25b9935f814d73e6e4bf559611d77
dhanansoneji <dhanansoneji@gmail.com>
2021-04-26 12:42:58 -0500

    Testing : added some unit test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,24 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import App from '../src/App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import {render, FindAllReturn} from '@testing-library/react-native';
 
+jest.useFakeTimers();
 it('renders correctly', () => {
-  renderer.create(<App />);
+  expect(renderer.create(<App />)).toMatchSnapshot();
+});
+
+it('children length', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree.children.length).toBe(3);
+});
+
+it('renders default elements', () => {
+  const {getAllByText} = render(<App />);
+
+  expect(getAllByText('Fetch Git Commits').length).toBe(1);
 });
