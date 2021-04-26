3595f77edfd94b812b220385b7613ea8c6b493ff
dhanansoneji <dhanansoneji@gmail.com>
2021-04-25 22:12:24 -0500

    Renamed : Renamed Project Name from (FatchGitCommit) to (Fetch Git Commit)

--- ios/FetchGitCommitsTests/FetchGitCommitsTests.m
+++ ios/FetchGitCommitsTests/FetchGitCommitsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface FatchGitCommitsTests : XCTestCase
+@interface FetchGitCommitsTests : XCTestCase
 
 @end
 
-@implementation FatchGitCommitsTests
+@implementation FetchGitCommitsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
